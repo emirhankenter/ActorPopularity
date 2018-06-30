9d38b0279467642d753aa372fd17e7bf0d4d85a6
Emirhan KENTER <emirhankenter@mek-intosh.local>
2018-06-30 14:19:18 +0300

    first commit

---
+++ PopularActors/ActorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActorViewController.swift
+//  PopularActors
+//
+//  Created by Emirhan KENTER on 30/06/2018.
+//  Copyright © 2018 Navek. All rights reserved.
+//
+
+import UIKit
+
+class ActorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
